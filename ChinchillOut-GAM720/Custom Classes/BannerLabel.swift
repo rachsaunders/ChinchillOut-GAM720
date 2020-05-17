93519b3f43c695aaf8c0d0b2531275c75cb92814
Rachel Saunders <rachelsaundersBA@gmail.com>
2020-05-17 13:18:10 +0100

    ERROR FIXED - Banner position.

--- ChinchillOut-GAM720/Custom Classes/BannerLabel.swift
+++ ChinchillOut-GAM720/Custom Classes/BannerLabel.swift
@@ -16,7 +16,10 @@ class BannerLabel: SKSpriteNode {
         super.init(texture: texture, color: UIColor.clear, size: texture.size())
         
         let label = SKLabelNode(fontNamed: GameConstants.StringConstants.gameFontName)
-        label.fontSize = 200.0
+        // SLIGHT ERROR/DESIGN FIX - CHANGED SIZE SO IT FITS BETTER
+        label.fontSize = 100.0
+        // SLIGHT ERROR/DESIGN FIX - CHANGED COLOUR SO IT CAN BE SEEN ON A WHITE BANNER BACKGROUND
+        label.fontColor = UIColor.red
         label.verticalAlignmentMode = .center
         label.text = title
         label.scale(to: size, width: false, multiplier: 0.3)
